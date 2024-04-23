6045b7f11d822628e268852ba88d18daa24d733b
yang aaa <yangfancheng1@gmail.com>
2024-04-22 21:07:04 -0400

    Fix description

--- src/main/resources/static/js/components/about.vue
+++ src/main/resources/static/js/components/about.vue
@@ -17,7 +17,6 @@
                     </div>
                     <h3>About</h3>
                     <p>
-<<<<<<< HEAD
                         Hello, my name is Yangfan. I am a software engineer with extensive experience in the Java ecosystem.
                         My expertise lies in designing and developing high-performing microservices deployed in the cloud.
                         I am very proficient in building web applications using technologies like Spring Boot, AWS, Docker, and MongoDB.
@@ -25,11 +24,9 @@
                         but I spent a lot of time in gaining advanced knowledge about various subjects even if they may not directly be used at work.
                         My focus is on continually challenging myself with new emerging technologies in order to build applications on them.
                         My recent interests are in Reactor, RSocket and anything AI related subjects like LLMs (Don't fight the trend!)
-=======
                         This project was my first attempt at building a chat-like application. Some of technologies I have incorporated in
                         this project are Spring Websocket, Spring MVC, Spring Redis (as session storage), & Spring JPA using MySQL. Source
                         can be found at <a href="https://github.com/YangfanCheng1/yangfan-chat" class="purple heavy">github</a>
->>>>>>> e10e9443ae4325bd8ba8572399d48a8076619644
                     </p>
                     <h3>Skill Breakdown</h3>
                     <b-progress :max="max" class="mb-3">
